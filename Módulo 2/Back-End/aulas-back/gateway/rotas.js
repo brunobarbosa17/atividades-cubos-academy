bb836faad40adf4af31ba3a5c66d267b1cbdf276
Bruno <brunnobarbosas@gmail.com>
2021-09-07 16:45:27 -0300

    ...

--- Módulo 2/Back-End/aulas-back/gateway/rotas.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const rotas = express()
-const pedidos = require('./controladores/pedidos')
-
-rotas.post('/criar-pedido', pedidos.criarPedido)
-rotas.get('/consultar-pedido/:id', pedidos.consultarPedido)
-
-module.exports = rotas;
